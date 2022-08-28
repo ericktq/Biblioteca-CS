dbb30e10e7f87a819e8bdfea1df396249f88f841
cheems-dev <daprimovaria@gmail.com>
2022-08-28 21:24:24 +0000

    💄 style: Add tabs

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,20 +1,19 @@
 @extends('adminlte::page')
 
-@section('title', 'Grupo Colitas Arequipa')
+@section('title', 'Biblioteca CS')
 
 @section('content_header')
-    <h1>Listado de publicaciónes</h1>
+<h1>Listado de publicaciónes</h1>
 @stop
 
 @livewireScripts
 
 @section('content')
-    @if (session('info'))
-        <div class="alert alert-success alert-dismissable">
-            <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
-            <strong>{{session('info')}}</strong>
-        </div>
-    @endif
-    @livewire('admin.posts-index')
+@if (session('info'))
+<div class="alert alert-success alert-dismissable">
+    <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+    <strong>{{session('info')}}</strong>
+</div>
+@endif
+@livewire('admin.posts-index')
 @stop
-
