1d00a36db7180a3604f312c909416698a3a81440
cheems-dev <daprimovaria@gmail.com>
2022-08-28 21:29:24 +0000

    ✨ feature: Add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\EventsController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,8 +11,12 @@ Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::get('category/{category}', [PostController::class, 'category'])->name('posts.category');
 
 Route::get('tag/{tag}', [PostController::class, 'tag'])->name('posts.tag');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('events', [EventsController::class, 'index'])->name('events.index');
+
 
 require __DIR__ . '/auth.php';
