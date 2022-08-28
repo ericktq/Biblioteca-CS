0ce7a766f188cbf872dda117cd512b1ec246ce21
cheems-dev <daprimovaria@gmail.com>
2022-08-28 23:19:46 +0000

    ✅ refact: Minor changes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,11 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
 
-
+    /**
+     * It returns the index view with the posts variable.
+     *
+     * @return A view called posts.index with a variable called posts.
+     */
     public function index()
     {
         $posts = Post::where('status', 2)->latest('id')->paginate(8);
@@ -18,6 +22,15 @@ class PostController extends Controller
         return view('posts.index', compact('posts'));
     }
 
+    /**
+     * It checks if the user is authorized to view the post, if not, it throws an exception. If the
+     * user is authorized, it fetches the posts with the same category as the post being viewed, and
+     * returns the view
+     *
+     * @param Post post The post object that we're passing to the view.
+     *
+     * @return A view of the post
+     */
     public function show(Post $post)
     {
 
@@ -31,6 +44,14 @@ class PostController extends Controller
         return view('posts.show', compact('post', 'similarity'));
     }
 
+    /**
+     * It takes a category object as an argument, and returns a view with the posts that belong to that
+     * category
+     *
+     * @param Category category This is the route parameter that we're passing to the method.
+     *
+     * @return A view with the posts and category
+     */
     public function category(Category $category)
     {
         $posts = Post::where('category_id', $category->id)
@@ -41,6 +62,13 @@ class PostController extends Controller
         return view('posts.category', compact('posts', 'category'));
     }
 
+    /**
+     * It takes a tag object as an argument, and returns a view with the posts that have that tag
+     *
+     * @param Tag tag This is the tag object that we're passing in.
+     *
+     * @return A view with the posts and tag
+     */
     public function tag(Tag $tag)
     {
         $posts =  $tag->posts()
